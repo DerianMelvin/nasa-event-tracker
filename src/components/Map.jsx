a167573dfc6ec1cd80f7739f249e34efcf002398
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-03-23 13:38:27 +0800

    Create: components\LocationInfoBox.jsx
    Import: LocationInfoBox > Map.jsx
    Import: { useState } from "react"
    Add: locationInfo state hook
    Add: onClick prop to <LocationMarker>
    Set: <LocationInfoBox>, locationInfo to return

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
-
+import LocationInfoBox from "./LocationInfoBox";
 
 const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null)
+
   const markers = eventData.map((data) => {
     if (data.categories[0].id === 8) {
       return (
         <LocationMarker
           lat={data.geometries[0].coordinates[1]}
           lng={data.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: data.id, title: data.title})}
         />
       );
     }
@@ -24,6 +28,7 @@ const Map = ({ eventData, center, zoom }) => {
       >
         {markers}
       </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
 };
