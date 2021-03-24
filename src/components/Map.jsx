1eb9b288256a7b34f6b68e474dae5bf7f5ab006d
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-03-24 23:28:31 +0800

    Import: ListGroup from "react-bootstrap/ListGroup" Add: useState [currentEvent] Modify: const markers functions Add: const adjustEvent function Set: <ListGroup>, <ListGroup.Item> to return

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,71 @@
-import { useState } from 'react';
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
 import LocationInfoBox from "./LocationInfoBox";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const Map = ({ eventData, center, zoom }) => {
-  const [locationInfo, setLocationInfo] = useState(null)
+  const [locationInfo, setLocationInfo] = useState(null);
+  const [currentEvent, setCurrentEvent] = useState(8);
 
   const markers = eventData.map((data) => {
-    if (data.categories[0].id === 8) {
+    const id = data.categories[0].id;
+    const coordinates = data.geometries[0].coordinates;
+
+    if (id === currentEvent) {
       return (
         <LocationMarker
-          lat={data.geometries[0].coordinates[1]}
-          lng={data.geometries[0].coordinates[0]}
-          onClick={() => setLocationInfo({ id: data.id, title: data.title})}
+          lat={coordinates[1]}
+          lng={coordinates[0]}
+          onClick={() => setLocationInfo({ id: data.id, title: data.title })}
         />
       );
     }
     return null;
   });
 
+  const adjustEvent = (id) => {
+    setCurrentEvent(id);
+  };
+
   return (
     <div className="map">
+      <ListGroup className="event-nav-list">
+        {/* Wildfires */}
+        {currentEvent === 8 ? (
+          <ListGroup.Item action active onClick={() => adjustEvent(8)}>
+            Wildfires
+          </ListGroup.Item>
+        ) : (
+          <ListGroup.Item action onClick={() => adjustEvent(8)}>
+            Wildfires
+          </ListGroup.Item>
+        )}
+
+        {/* Sea and Lake Ice */}
+        {currentEvent === 15 ? (
+          <ListGroup.Item action active onClick={() => adjustEvent(15)}>
+            Sea and Lake Ice
+          </ListGroup.Item>
+        ) : (
+          <ListGroup.Item action onClick={() => adjustEvent(15)}>
+            Sea and Lake Ice
+          </ListGroup.Item>
+        )}
+
+        {/* Volcanoes */}
+        {currentEvent === 12 ? (
+          <ListGroup.Item action active onClick={() => adjustEvent(12)}>
+            Volcanoes
+          </ListGroup.Item>
+        ) : (
+          <ListGroup.Item action onClick={() => adjustEvent(12)}>
+            Volcanoes
+          </ListGroup.Item>
+        )}
+        {/* TODO: handle an abnormal array value ( !Array.isArray(coordinates[0]) ) */}
+      </ListGroup>
+
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDEMRNXSoHXfQjxH4mrrsNn_QRR3cqb6oA" }}
         defaultCenter={center}
@@ -28,6 +73,7 @@ const Map = ({ eventData, center, zoom }) => {
       >
         {markers}
       </GoogleMapReact>
+
       {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
