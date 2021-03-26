cc18f7fecc2039ba0dcf5228f790fd99e65e4caa
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-03-26 11:45:58 +0800

    Import:   - fireIcon from "@iconify/icons-icomoon-free/fire";   - waterIcon from '@iconify/icons-ion/water';   - volcano15 from '@iconify/icons-maki/volcano-15'; Add: <InlineIcon> to each corresponding <ListGroup.Item> Add: .event-list-item to <ListGroup.Item> Add: .list-item-icon to <InlineIcon>

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,43 +1,81 @@
 import ListGroup from "react-bootstrap/ListGroup";
 
+import { InlineIcon } from '@iconify/react';
+import fireIcon from "@iconify/icons-icomoon-free/fire";
+import waterIcon from '@iconify/icons-ion/water';
+import volcano15 from '@iconify/icons-maki/volcano-15';
+
 const EventList = ({ adjustEvent, currentEvent }) => {
   return (
     <ListGroup className="event-nav-list">
       {/* Wildfires */}
       {currentEvent === 8 ? (
-          <ListGroup.Item action active onClick={() => adjustEvent(8)}>
-            Wildfires
-          </ListGroup.Item>
-        ) : (
-          <ListGroup.Item action onClick={() => adjustEvent(8)}>
-            Wildfires
-          </ListGroup.Item>
-        )}
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          active
+          onClick={() => adjustEvent(8)}
+        >
+          <InlineIcon className="list-item-icon" icon={fireIcon} />
+          Wildfires
+        </ListGroup.Item>
+      ) : (
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          onClick={() => adjustEvent(8)}
+        >
+          <InlineIcon className="list-item-icon" icon={fireIcon} />
+          Wildfires
+        </ListGroup.Item>
+      )}
 
-        {/* Sea and Lake Ice */}
-        {currentEvent === 15 ? (
-          <ListGroup.Item action active onClick={() => adjustEvent(15)}>
-            Sea and Lake Ice
-          </ListGroup.Item>
-        ) : (
-          <ListGroup.Item action onClick={() => adjustEvent(15)}>
-            Sea and Lake Ice
-          </ListGroup.Item>
-        )}
+      {/* Sea and Lake Ice */}
+      {currentEvent === 15 ? (
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          active
+          onClick={() => adjustEvent(15)}
+        >
+          <InlineIcon className="list-item-icon" icon={waterIcon} />
+          Sea and Lake Ice
+        </ListGroup.Item>
+      ) : (
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          onClick={() => adjustEvent(15)}
+        >
+          <InlineIcon className="list-item-icon" icon={waterIcon} />
+          Sea and Lake Ice
+        </ListGroup.Item>
+      )}
 
-        {/* Volcanoes */}
-        {currentEvent === 12 ? (
-          <ListGroup.Item action active onClick={() => adjustEvent(12)}>
-            Volcanoes
-          </ListGroup.Item>
-        ) : (
-          <ListGroup.Item action onClick={() => adjustEvent(12)}>
-            Volcanoes
-          </ListGroup.Item>
-        )}
-        {/* TODO: handle an abnormal array value ( !Array.isArray(coordinates[0]) ) */}
+      {/* Volcanoes */}
+      {currentEvent === 12 ? (
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          active
+          onClick={() => adjustEvent(12)}
+        >
+          <InlineIcon className="list-item-icon" icon={volcano15} />
+          Volcanoes
+        </ListGroup.Item>
+      ) : (
+        <ListGroup.Item
+          className="event-list-item"
+          action
+          onClick={() => adjustEvent(12)}
+        >
+          <InlineIcon className="list-item-icon" icon={volcano15} />
+          Volcanoes
+        </ListGroup.Item>
+      )}
+      {/* TODO: handle an abnormal array value ( !Array.isArray(coordinates[0]) ) */}
     </ListGroup>
-  )
-}
+  );
+};
 
-export default EventList
+export default EventList;
