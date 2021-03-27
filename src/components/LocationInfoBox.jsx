7131e88b7d2da3100dc6e7c8970306dd15d8d4a5
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-03-27 12:45:41 +0800

    Add RegExp to remove "Wildfire/-s" Add map() method to list all link sources Adjust <h2>, <li> return values

--- src/components/LocationInfoBox.jsx
+++ src/components/LocationInfoBox.jsx
@@ -1,13 +1,28 @@
 const LocationInfoBox = ({ info }) => {
+  // Remove "Wildfire / Wildfires" from title
+  const regex = /(Wildfires|Wildfire)|[-]/gi;
+  const adjustTitle = info.title.replace(regex, "");
+
+  // List all available sources if 1 or more exist
+  const listSource = info.source.map((data) => {
+    return (
+      <a href={data.url} target="_blank" rel="noopener noreferrer">
+        {data.id + " "}
+      </a>
+    );
+  });
+
   return (
     <div className="location-info">
-      <h2>Event Location Info</h2>
+      <h2>
+        <strong>{adjustTitle}</strong>
+      </h2>
       <ul>
-        <li>ID: <strong>{info.id}</strong></li>
-        <li>TITLE: <strong>{info.title}</strong></li>
+        <li>ID: {info.id}</li>
+        <li>Sources: {listSource} </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default LocationInfoBox
+export default LocationInfoBox;
