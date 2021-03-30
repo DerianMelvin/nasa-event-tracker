f9d502b9c1455b60de1c26033a428088816d3ec4
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-03-30 10:44:01 +0800

    - Import waterIcon, volcano15 icons - Add currentEvent prop - Display icons based on currentEvent value

--- src/components/LocationMarker.jsx
+++ src/components/LocationMarker.jsx
@@ -1,10 +1,18 @@
 import { Icon } from "@iconify/react";
 import fireIcon from "@iconify/icons-icomoon-free/fire";
+import waterIcon from "@iconify/icons-ion/water";
+import volcano15 from "@iconify/icons-maki/volcano-15";
 
-const LocationMarker = ({ lat, lng, onClick }) => {
+const LocationMarker = ({ lat, lng, currentEvent, onClick }) => {
   return (
     <div className="location-marker" onClick={onClick}>
-      <Icon icon={fireIcon} className="location-icon" />
+      {currentEvent === 8 ? (
+        <Icon icon={fireIcon} className="location-icon" />
+      ) : currentEvent === 12 ? (
+        <Icon icon={volcano15} className="location-icon" />
+      ) : (
+        <Icon icon={waterIcon} className="location-icon" />
+      )}
     </div>
   );
 };
